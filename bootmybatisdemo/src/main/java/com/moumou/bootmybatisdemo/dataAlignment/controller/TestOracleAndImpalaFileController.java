37687a2d6e303b9bf866b87bc225c6a2feb32f30
yangweijin <yangweijin@sunline.cn>
2019-09-23 16:43:26 +0800

    修改返回值json格式

--- bootmybatisdemo/src/main/java/com/moumou/bootmybatisdemo/dataAlignment/controller/TestOracleAndImpalaFileController.java
+++ bootmybatisdemo/src/main/java/com/moumou/bootmybatisdemo/dataAlignment/controller/TestOracleAndImpalaFileController.java
@@ -1,11 +1,15 @@
 package com.moumou.bootmybatisdemo.dataAlignment.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.moumou.bootmybatisdemo.dataAlignment.service.TestOracleAndImpalaFileService;
+import com.moumou.bootmybatisdemo.util.JsonResult;
 
 @RestController
 @RequestMapping("/oracleandimpalafile")
@@ -14,22 +18,62 @@ public class TestOracleAndImpalaFileController {
 	@Autowired
 	private TestOracleAndImpalaFileService testOracleAndImpalaFileService;
 	@RequestMapping(value="/oraclefile",method = RequestMethod.GET)
-	public String oracleFile() {
-		return testOracleAndImpalaFileService.oracleFileDdl();
+	public JsonResult oracleFile() {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		String s = testOracleAndImpalaFileService.oracleFileDdl();
+		if("success".equals(s)) {
+			map.put("status", "success");
+	        map.put("msg", "生成oracle的ddl完成"); 
+		}else {
+			map.put("status", "false");
+	        map.put("msg", "生成失败"); 
+		}
+		return new JsonResult(map);
 	}
 	
 	@RequestMapping(value="/impalafile",method = RequestMethod.GET)
-	public String impalaFile() {
-		return testOracleAndImpalaFileService.impalaFileDdl();
+	public JsonResult impalaFile() {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		String s = testOracleAndImpalaFileService.impalaFileDdl();
+		if("success".equals(s)) {
+			map.put("status", "success");
+			map.put("msg", "生成impala的ddl完成");
+		}else {
+			map.put("status", "false");
+	        map.put("msg", "生成失败"); 
+		}
+		return new JsonResult(map);
 	}
 	
 	@RequestMapping(value="/oraclejobfile",method = RequestMethod.GET)
-	public String oracleJobFile() {
-		return testOracleAndImpalaFileService.oracleJobFile();
+	public JsonResult oracleJobFile() {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		String s = testOracleAndImpalaFileService.oracleJobFile();
+		if("success".equals(s)) {
+			map.put("status", "success");
+			map.put("msg", "生成oracle的job作业完成");
+		}else {
+			map.put("status", "false");
+	        map.put("msg", "生成失败"); 
+		}
+		return new JsonResult(map);
 	}
 	
 	@RequestMapping(value="/impalajobfile",method = RequestMethod.GET)
-	public String impalaJobFile() {
-		return testOracleAndImpalaFileService.impalaJobFile();
+	public JsonResult impalaJobFile() {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		String s = testOracleAndImpalaFileService.impalaJobFile();
+		if("success".equals(s)) {
+			map.put("status", "success");
+			map.put("msg", "生成impala的job作业完成");
+		}else {
+			map.put("status", "false");
+	        map.put("msg", "生成失败"); 
+		}
+		return new JsonResult(map);
 	}
 }
