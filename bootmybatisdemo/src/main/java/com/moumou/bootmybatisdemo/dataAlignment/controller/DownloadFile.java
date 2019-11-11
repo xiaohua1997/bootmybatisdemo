7b58b9cd50d0b2968bfd6278ebcea350d2416b33
xiaohua <1137781117@qq.com>
2019-11-11 10:38:03 +0800

    文件下载修改

--- bootmybatisdemo/src/main/java/com/moumou/bootmybatisdemo/dataAlignment/controller/DownloadFile.java
+++ bootmybatisdemo/src/main/java/com/moumou/bootmybatisdemo/dataAlignment/controller/DownloadFile.java
@@ -1,15 +1,18 @@
 package com.moumou.bootmybatisdemo.dataAlignment.controller;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,74 +26,79 @@ import com.moumou.bootmybatisdemo.serviceinterfaceimp.terminal.ScheduleJobs;
 @RestController
 @RequestMapping("/OracleAndImpala")
 public class DownloadFile {
-	
+
 	@RequestMapping("/downloadFile")
 	private String downloadFile(HttpServletResponse response) {
-		 String downloadFilePath = "C:\\Users\\Administrator\\Desktop\\sqoop_project\\sqoop_project.zip";//被下载的文件在服务器中的路径,
-         String fileName = "sqoop_project.zip";//被下载文件的名称
-         
-         File file = new File(downloadFilePath);
-         if(file.exists()) {
-        	 response.setContentType("application/force-download");// 设置强制下载不打开
-        	 
-        	 response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);
-        	 byte[] buffer = new byte[1024];
-        	 FileInputStream fis = null;
-        	 BufferedInputStream bis = null;
-        	 try {
-        		 fis = new FileInputStream(file);  
-        		 bis = new BufferedInputStream(fis);
-        		 OutputStream outputStream = response.getOutputStream();
-        		 int i = bis.read(buffer);
-        		 while (i != -1) {
-                     outputStream.write(buffer, 0, i);
-                     i = bis.read(buffer);
-                 }
-        		 return "下载成功";
-        	 } catch (Exception e) {
-                 e.printStackTrace();
-             }finally {
-                 if (bis != null) {
-                     try {
-                         bis.close();
-                     } catch (IOException e) {
-                         e.printStackTrace();
-                     }
-                 }
-                 if (fis != null) {
-                     try {
-                         fis.close();
-                     } catch (IOException e) {
-                         e.printStackTrace();
-                     }
-                 }
-             }
-         }
-        return "下载失败"; 
+		String downloadFilePath = "C:\\Users\\Administrator\\Desktop\\sqoop_project\\sqoop_project.zip";// 被下载的文件在服务器中的路径,
+		String fileName = "sqoop_project.zip";// 被下载文件的名称
+
+		File file = new File(downloadFilePath);
+		if (file.exists()) {
+			response.setContentType("application/force-download");// 设置强制下载不打开
+
+			response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);
+			byte[] buffer = new byte[1024];
+			FileInputStream fis = null;
+			BufferedInputStream bis = null;
+			try {
+				fis = new FileInputStream(file);
+				bis = new BufferedInputStream(fis);
+				OutputStream outputStream = response.getOutputStream();
+				int i = bis.read(buffer);
+				while (i != -1) {
+					outputStream.write(buffer, 0, i);
+					i = bis.read(buffer);
+				}
+				return "下载成功";
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if (bis != null) {
+					try {
+						bis.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				if (fis != null) {
+					try {
+						fis.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		return "下载失败";
 	}
-	
+
 	@Autowired
 	private FileService fileService;
 	@Autowired
 	private FileToZip fileToZip;
-	
+
 	@RequestMapping("/downfile")
-	public void down(HttpServletResponse response) throws UnsupportedEncodingException {
+	public void down(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
 		String dir = URLDecoder.decode(ScheduleJobs.class.getClassLoader().getResource("").getPath(), "utf-8")
-				+"/sqoop_project";
-		Map<String, Object> map = new HashMap<String, Object>();
-		String fileName1 = "sqoop_project";
-		String downloadFilePath = dir+"/sqoop_project.zip";
-		String fileName2 = "sqoop_project.zip";
+				+ "/hive_q";
+		String downloadFilePath = dir + "/hive_q.zip";
+		String fileName = "hive_q";
 		File file = new File(downloadFilePath);
-		if(file.exists()) {
-			System.out.println(dir + "目录下存在名字为:" + fileName1 + ".zip" + "打包文件.");
-			boolean flag3 = file.delete();
-			if(flag3) System.out.println("删除成功");
+		if (file.exists()) {
+			System.out.println(dir + "目录下存在名字为:" + fileName + ".zip" + "打包文件.");
+			boolean flag1 = file.delete();
+			if (flag1)
+				System.out.println("删除成功");
 		}
-		boolean flag1 = fileToZip.fileToZip(dir, dir, fileName1);
-		if(flag1) System.out.println("打包成功");
-        boolean flag2 = fileService.downloadFile(response, downloadFilePath, fileName2);
+		boolean flag2 = fileToZip.fileToZip(dir, dir, fileName);
+		if (flag2)
+			System.out.println("打包成功");
+		boolean flag3 = fileService.downloadFile(request, response, fileName);
+//		boolean flag3 = fileService.downloadFile(request, response, downloadFilePath, fileName + ".zip" );
+		if (flag3)
+			System.out.println("下载成功");
 	}
+
 	
+
 }
--- bootmybatisdemo/src/main/java/com/moumou/bootmybatisdemo/dataAlignment/service/impl/FileServiceImpl.java
+++ bootmybatisdemo/src/main/java/com/moumou/bootmybatisdemo/dataAlignment/service/impl/FileServiceImpl.java
@@ -1,5 +1,10 @@
 package com.moumou.bootmybatisdemo.dataAlignment.service.impl;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +12,7 @@ import org.springframework.stereotype.Service;
 import com.moumou.bootmybatisdemo.dataAlignment.service.FileService;
 import com.moumou.bootmybatisdemo.serviceinterfaceimp.terminal.DownLoadFile;
 import com.moumou.bootmybatisdemo.serviceinterfaceimp.terminal.FileToZip;
+import com.moumou.bootmybatisdemo.serviceinterfaceimp.terminal.ScheduleJobs;
 
 @Service
 public class FileServiceImpl implements FileService {
@@ -38,13 +44,33 @@ public class FileServiceImpl implements FileService {
 	 * fileName 被下载文件的名称      例如："sqoop_project.zip"
 	 */
 	@Override
-	public boolean downloadFile(HttpServletResponse response, String downloadFilePath, String fileName) {
-		DownLoadFile downLoadFile = new DownLoadFile();
-		boolean flag = downLoadFile.downloadFile(response, downloadFilePath, fileName);
-		if (flag) {
-			System.out.println("文件下载成功!");
-		} else {
-			System.out.println("文件下载失败!");
+	public boolean downloadFile(HttpServletRequest request, HttpServletResponse response,String fileName) {
+		String dir;
+		boolean flag = false;
+		try {
+			dir = URLDecoder.decode(ScheduleJobs.class.getClassLoader().getResource("").getPath(), "utf-8")
+					+ "/hive_q";
+			String downloadFilePath = dir + "/hive_q.zip";
+			File file = new File(downloadFilePath);
+			if (file.exists()) {
+				System.out.println(dir + "目录下存在名字为:" + fileName + ".zip" + "打包文件.");
+				boolean flag1 = file.delete();
+				if (flag1)
+					System.out.println("删除成功");
+			}
+			boolean flag2 = fileToZip(dir, dir, fileName);
+			if (flag2)
+				System.out.println("打包成功");
+			DownLoadFile downLoadFile = new DownLoadFile();
+			boolean flag3 = downLoadFile.downFile(request, response, downloadFilePath, fileName + ".zip" );
+			if (flag3) {
+				System.out.println("文件下载成功!");
+			} else {
+				System.out.println("文件下载失败!");
+			}
+			flag = true;
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
 		}
 		return flag;
 	}
